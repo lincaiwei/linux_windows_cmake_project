b63df6d55c4dc52440b9eb1d547d5245fbd84c8d
caiwei.lin <cwlin50@hotmail.com>
2024-11-30 17:41:16 +0800

    cmake指定输出路径

--- build.bat
+++ build.bat
@@ -2,7 +2,22 @@
 @echo off
 .\git_version.sh
 
+set TOPDIR=%CD%
+set build_out_dir=%TOPDIR%\output
+set install_dir=%TOPDIR%\bin
+echo "TOPDIR = %TOPDIR%"
+echo "build_out_dir = %build_out_dir%"
+echo "install_dir = %install_dir%"
+
+set software_version=V1.0.0
+
 mkdir output
-cd .\output\
-cmake -G "MinGW Makefiles" ..
-make
+::mkdir bin
+::cd .\output\
+::cmake -G "MinGW Makefiles" ..
+::make
+::cmake -G "MinGW Makefiles" -S %TOPDIR% -B %build_out_dir% --install-prefix %install_dir% -DSW_VERSION=%software_version% -DCMAKE_VERBOSE_MAKEFILE=OFF
+cmake -G "MinGW Makefiles" -S %TOPDIR% -B %build_out_dir% -DCMAKE_INSTALL_PREFIX=%install_dir% -DSW_VERSION=%software_version% -DCMAKE_VERBOSE_MAKEFILE=OFF
+cmake --build %build_out_dir%
+echo "cmake install"
+cmake --install %build_out_dir%
--- version.h
+++ version.h
@@ -2,10 +2,10 @@
 #define _VERSION__H_
 
 #define SW_VERSION    "v1.00.0.0"
-#define GIT_VERSION "d33075d-dev"
+#define GIT_VERSION "b5c8b1c-dev"
 #define GIT_BRANCH "main"
-#define GIT__BRANCH__VERSION "main - d33075d-dev"
-#define BUILD_DATE " cwlin Nov 24 2024 22:47:36"
+#define GIT__BRANCH__VERSION "main - b5c8b1c-dev"
+#define BUILD_DATE " cwlin Nov 30 2024 17:50:05"
 
 #define SW_FW_NAME   			"app"
 
